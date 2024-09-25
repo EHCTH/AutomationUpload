bdb1bf9561017b2c26633d4186f059599a761dde
EHCTH <lees90896@gmail.com>
2024-09-25 14:27:34 +0900

    feat: AlgorithmTag actual

--- src/main/java/domain/algorithm/AlgorithmTag.java
+++ src/main/java/domain/algorithm/AlgorithmTag.java
@@ -1,2 +1,31 @@
-package domain.algorithm;public enum AlgorithmTag {
+package domain.algorithm;
+
+import java.util.Arrays;
+
+public enum AlgorithmTag {
+    IMPLEMENT("구현", "IMPLEMENTATION"),
+    UNKNOWN("UNKNOWN", "UNKNOWN");
+    private final String key;
+    private final String value;
+    private final static AlgorithmTag[] TAGS = values();
+    AlgorithmTag(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+    private String getTag() {
+        return value;
+    }
+    private boolean findKey(String key) {
+        return this.key.equals(key);
+    }
+    private static String unknownTag() {
+        return UNKNOWN.value;
+    }
+    public static String findTag(String key) {
+        return Arrays.stream(TAGS)
+                .filter((tag) -> tag.findKey(key))
+                .findFirst()
+                .map(AlgorithmTag::getTag)
+                .orElseGet(AlgorithmTag::unknownTag);
+    }
 }

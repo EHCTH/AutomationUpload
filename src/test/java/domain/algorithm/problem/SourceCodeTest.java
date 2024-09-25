e134f5b33848a0eb0fe58e779cd72eb415998a23
EHCTH <lees90896@gmail.com>
2024-09-25 17:20:53 +0900

    test: SourceCode Test add * feat: presentSourceCodeTest() 추가 * feat: absentSourceCodeTest() 추가

--- src/test/java/domain/algorithm/problem/SourceCodeTest.java
+++ src/test/java/domain/algorithm/problem/SourceCodeTest.java
@@ -1,2 +1,23 @@
-package domain.algorithm.problem;public class sourceCodeTest {
+package domain.algorithm.problem;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class SourceCodeTest {
+    @Test
+    public void presentSourceCodeTest() {
+        SourceCode sourceCodeObj = new SourceCode(
+                "a, b = map(int, input().split())\n" +
+                        "print(a+b)\n");
+        String sourceCode = sourceCodeObj.getSourceCode();
+        assertThat(sourceCode).isEqualTo(
+                "a, b = map(int, input().split())\n" +
+                        "print(a+b)\n");
+    }
+    @Test
+    public void absentSourceCodeTest() {
+        assertThatThrownBy(() -> new SourceCode("")
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 해당 소스 코드가 없습니다");
+    }
 }

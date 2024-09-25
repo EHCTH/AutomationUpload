d5b81d285ed0d133c9bf2b40f8c628ff97cb95f1
EHCTH <lees90896@gmail.com>
2024-09-25 14:24:07 +0900

    feat: AlgorithmTat Test 작성 * feat: isPresentAlgorithmTagTest (태그가 있는 경우) * feat: isAbsentAlgorithmTagTest (태그가 없는 경우)

--- src/test/java/domain/algorithm/AlgorithmTagTest.java
+++ src/test/java/domain/algorithm/AlgorithmTagTest.java
@@ -1,2 +1,18 @@
-package domain.algorithm;public class AlgorithmTagTest {
+package domain.algorithm;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class AlgorithmTagTest {
+    @Test
+    public void isPresentAlgorithmTagTest() {
+        String algorithmTag = AlgorithmTag.findTag("구현");
+        assertThat(algorithmTag).isEqualTo("IMPLEMENTATION");
+    }
+
+    @Test
+    public void isAbsentAlgorithmTagTest() {
+        String algorithmTag = AlgorithmTag.findTag("없는 태그");
+        assertThat(algorithmTag).isEqualTo("UNKNOWN");
+    }
 }

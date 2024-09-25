4b1f19dcc3bc2c8dff310fedbac7c519dacd8bd8
EHCTH <lees90896@gmail.com>
2024-09-25 17:52:00 +0900

    test: ProblemTest Add * feat: Problem 통합 테스트 작성

--- src/test/java/domain/algorithm/problem/ProblemTest.java
+++ src/test/java/domain/algorithm/problem/ProblemTest.java
@@ -1,12 +1,32 @@
-package domain.algorithm;
+package domain.algorithm.problem;
 
 import org.junit.jupiter.api.Test;
+
+
 import static org.assertj.core.api.Assertions.*;
 public class ProblemTest {
     @Test
     public void test() {
-        Problem problem = new problem();
-        String sourceCode = problem.sourceCdoe();
+        Problem problem = new Problem.Builder()
+                .problemNumber("1000")
+                .sourceCode("a, b = map(int, input().split())\n" +
+                        "print(a+b)\n")
+                .extension("py")
+                .algorithmTag("수학")
+                .build();
+
+        String problemNumber = problem.getNumber();
+        String sourceCode = problem.getSourceCode();
+        String fileTxt = problem.getFileName();
+        String algorithmTag = problem.getTag();
+
+        assertThat(problemNumber).isEqualTo("1000");
+        assertThat(sourceCode).isEqualTo(
+                "a, b = map(int, input().split())\n" +
+                "print(a+b)\n"
+        );
+        assertThat(fileTxt).isEqualTo("1000.py");
+        assertThat(algorithmTag).isEqualTo("MATHEMATICS");
 
 
     }

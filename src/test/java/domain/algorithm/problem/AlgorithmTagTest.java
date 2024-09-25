3596cefe51b3ad6d2bd8b9abb458610afc88333a
EHCTH <lees90896@gmail.com>
2024-09-25 15:41:19 +0900

    test: 객체를 리턴하게 수정 refactor: 객체를 리턴하게 수정

--- src/test/java/domain/algorithm/problem/AlgorithmTagTest.java
+++ src/test/java/domain/algorithm/problem/AlgorithmTagTest.java
@@ -7,13 +7,15 @@ import static org.assertj.core.api.Assertions.*;
 public class AlgorithmTagTest {
     @Test
     public void isPresentAlgorithmTagTest() {
-        String algorithmTag = AlgorithmTag.findTag("구현");
-        assertThat(algorithmTag).isEqualTo("IMPLEMENTATION");
+        AlgorithmTag algorithmTag = AlgorithmTag.findTag("구현");
+        String tag = algorithmTag.getTag();
+        assertThat(tag).isEqualTo("IMPLEMENTATION");
     }
 
     @Test
     public void isAbsentAlgorithmTagTest() {
-        String algorithmTag = AlgorithmTag.findTag("없는 태그");
-        assertThat(algorithmTag).isEqualTo("UNKNOWN");
+        AlgorithmTag algorithmTag = AlgorithmTag.findTag("없는 태그");
+        String tag = algorithmTag.getTag();
+        assertThat(tag).isEqualTo("UNKNOWN");
     }
 }

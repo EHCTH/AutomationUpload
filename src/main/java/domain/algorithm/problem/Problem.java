9a96078d7867ac974fa07180b505e7160b2e7f9c
EHCTH <lees90896@gmail.com>
2024-09-25 17:53:00 +0900

    feat: Problem Acture Add * feat: Builder Pattern 적용

---
+++ src/main/java/domain/algorithm/problem/Problem.java
@@ -0,0 +1,69 @@
+package domain.algorithm.problem;
+
+public class Problem {
+    private final ProblemNumber problemNumber;
+    private final SourceCode sourceCode;
+    private final AlgorithmTag algorithmTag;
+    private final Extension extension;
+
+    private Problem(Builder builder) {
+        this.problemNumber = builder.problemNumber;
+        this.sourceCode = builder.sourceCode;
+        this.algorithmTag = builder.algorithmTag;
+        this.extension = builder.extension;
+    }
+    public String getSourceCode() {
+        return sourceCode.getSourceCode();
+    }
+
+    public String getNumber() {
+        return problemNumber.getNumber();
+    }
+
+    public String getTag() {
+        return algorithmTag.getTag();
+    }
+
+    public String getExtension() {
+        return extension.getExtension();
+    }
+
+    public String getFileName() {
+        return getNumber() + "." + getExtension();
+    }
+
+
+
+    public static class Builder {
+        private ProblemNumber problemNumber;
+        private SourceCode sourceCode;
+        private AlgorithmTag algorithmTag;
+        private Extension extension;
+
+        public Builder() {}
+
+        public Builder problemNumber(String problemNumber) {
+            this.problemNumber = new ProblemNumber(problemNumber);
+            return this;
+        }
+
+        public Builder sourceCode(String sourceCode) {
+            this.sourceCode = new SourceCode(sourceCode);
+            return this;
+        }
+
+        public Builder algorithmTag(String algorithmTag) {
+            this.algorithmTag = AlgorithmTag.findTag(algorithmTag);
+            return this;
+        }
+
+        public Builder extension(String extension) {
+            this.extension = Extension.findExtension(extension);
+            return this;
+        }
+
+        public Problem build() {
+            return new Problem(this);
+        }
+    }
+}

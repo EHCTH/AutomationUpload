ebfbf9dc9db0b6640e11088b75d229e1dfb59d22
EHCTH <lees90896@gmail.com>
2024-09-26 17:10:38 +0900

    fix: lowerCase -> UpperCase Constant * fix: 소문자에서 대문자로 변경

--- src/main/java/infrastructure/url/Url.java
+++ src/main/java/infrastructure/url/Url.java
@@ -1,8 +1,8 @@
 package infrastructure.url;
 
 public class Url {
-    private static final String loginUrl = "https://www.acmicpc.net/login?next=%2F";
+    private static final String LOGIN_URL = "https://www.acmicpc.net/login?next=%2F";
     public static String getLoginUrl() {
-        return loginUrl;
+        return LOGIN_URL;
     }
 }

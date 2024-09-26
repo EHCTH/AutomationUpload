0a7324556720dae5a730a02a3d9a79dc03172df2
EHCTH <lees90896@gmail.com>
2024-09-26 18:32:51 +0900

    refactor : 정적 메서드 -> 인스턴스 메서드 * refactor : 정적메서드-> 인스턴스 메서드

--- src/main/java/application/service/UserService.java
+++ src/main/java/application/service/UserService.java
@@ -4,7 +4,7 @@ import domain.user.User;
 import util.DotenvUtil;
 
 public class UserService {
-    public static User createUser() {
+    public User createUser() {
         String id = DotenvUtil.getID();
         String pass = DotenvUtil.getPass();
         return new User(id, pass);

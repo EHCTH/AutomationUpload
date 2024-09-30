9ff4985f4d4b43f53570140b28f2e698cca8a92c
EHCTH <lees90896@gmail.com>
2024-09-30 17:39:13 +0900

    feat : All Elements return method add

--- src/main/java/infrastructure/selenium/Driver/DriverController.java
+++ src/main/java/infrastructure/selenium/Driver/DriverController.java
@@ -1,7 +1,12 @@
 package infrastructure.selenium.Driver;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
 
 public interface DriverController {
     void get(String url);
@@ -9,4 +14,6 @@ public interface DriverController {
     void writeText(By by, String text);
     void clickButton(By by);
     void quit();
+    Set<Cookie> getCookie();
+    List<WebElement> findElements(By by);
 }
--- src/main/java/infrastructure/selenium/Driver/WaitDriverController.java
+++ src/main/java/infrastructure/selenium/Driver/WaitDriverController.java
@@ -3,4 +3,5 @@ package infrastructure.selenium.Driver;
 public interface WaitDriverController {
     void visibilityOfElementLocated(String selector);
     void elementToBeClickable(String selector);
+    void visibilityOfAllElementsLocatedBy(String selector);
 }

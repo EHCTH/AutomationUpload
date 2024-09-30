85cf7d41f4b62c3ba65cbf52d288f01ff1c9092b
EHCTH <lees90896@gmail.com>
2024-09-30 17:41:01 +0900

    feat: Cookie 관리 클래스 추가 * feat: login cookie 관리

---
+++ src/main/java/domain/cookie/SeleniumCookie.java
@@ -0,0 +1,16 @@
+package domain.cookie;
+
+import org.openqa.selenium.Cookie;
+
+import java.util.Set;
+
+public class SeleniumCookie {
+    private final Set<Cookie> cookies;
+
+    public SeleniumCookie(Set<Cookie> cookie) {
+        this.cookies = cookie;
+    }
+    public Set<Cookie> getCookies() {
+        return cookies;
+    }
+}

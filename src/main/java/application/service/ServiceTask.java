d2843d4518fdd2924237f75a0fc4c417e55a9c07
EHCTH <lees90896@gmail.com>
2024-10-02 12:04:27 +0900

    feat: ProblemExtractor add * feat: ProbemExtractor add

--- src/main/java/application/service/ServiceTask.java
+++ src/main/java/application/service/ServiceTask.java
@@ -1,13 +1,23 @@
 package application.service;
 
+import application.dto.PageElements;
+import application.dto.ProblemInfoDto;
+import application.service.extract.ExtractPageElementsFactory;
 import application.service.login.LoginServiceManage;
 import application.service.profile.ProfileManage;
+import domain.algorithm.problem.AlgorithmTag;
+import domain.algorithm.problem.Problem;
 import domain.cookie.SeleniumCookie;
+import infrastructure.parse.*;
 import infrastructure.selenium.Driver.*;
 import infrastructure.url.Url;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServiceTask implements TaskManger {
     private final Logger logger = LoggerFactory.getLogger(ServiceTask.class);
@@ -15,15 +25,19 @@ public class ServiceTask implements TaskManger {
     private final DriverController driver;
     private final WaitDriverController waitDriver;
     private final ProfileManage profileService;
+    private final ExtractorManager problemExtractor;
+
     public ServiceTask(LoginServiceManage loginService,
                        DriverController driver,
                        WaitDriverController waitDriver,
-                       ProfileManage profileService
-                       ) {
+                       ProfileManage profileService,
+                       ExtractorManager problemExtractor
+    ) {
         this.loginService = loginService;
         this.driver = driver;
         this.waitDriver = waitDriver;
         this.profileService = profileService;
+        this.problemExtractor = problemExtractor;
     }
 
     @Override
@@ -42,10 +56,16 @@ public class ServiceTask implements TaskManger {
             SeleniumCookie cookies = new SeleniumCookie(driver.getCookie());
             enterMyPage();
             enterMysSolvedPage();
-        } catch (InterruptedException e) {
+            List<String> problemLink = extractProblemLink(cookies);
+            ProblemInfoDto problemInfoDto = extractProblemInfoDto(problemLink, cookies);
+            for (Problem problem : problemInfoDto.getProblemInfo()) {
+                System.out.println(problem);
+            }
+
+
+        } catch (InterruptedException | IOException e) {
             throw new RuntimeException(e);
-        }
-        finally {
+        } finally {
             quit();
         }
     }
@@ -67,5 +87,51 @@ public class ServiceTask implements TaskManger {
     public void enterMysSolvedPage() {
         profileService.enterMySolvePage(driver, waitDriver);
         logger.info("나의 맞은 문제 클릭 완료");
+    }
+
+    @Override
+    public List<String> extractProblemLink(SeleniumCookie cookie) {
+        PageElements pages = ExtractPageElementsFactory.extractPage(driver, waitDriver);
+        PageLinks pageLinks = new PageLinks(pages);
+        Parse parse = new Parse(new CookieManager());
+        List<String> problemInfo = new ArrayList<>();
+        List<String> hrefModelPage = pageLinks.getHrefModel();
+        try {
+            for (String url : hrefModelPage) {
+                parse.connectAndInitCookies(url, cookie);
+                List<String> problemLink = problemExtractor.extractProblemProfile(parse);
+                problemInfo.addAll(problemLink);
+            }
+        } catch (IOException e) {
+            logger.warn(e.getMessage());
+        }
+        return problemExtractor.extractProblemLink(problemInfo);
+    }
+
+    @Override
+    public ProblemInfoDto extractProblemInfoDto(List<String> problemInfo, SeleniumCookie cookies) throws IOException {
+        logger.info("enter DTO {}", problemInfo);
+        List<Problem> ret = new ArrayList<>();
+        Parse parse = new Parse(new CookieManager());
+        for (String url : problemInfo) {
+            driver.get(url);
+            String algorithmTag = profileService.findAlgorithmTag(driver, waitDriver);
+            profileService.enterMySolveSubmissionPage(driver, waitDriver);
+            WebElement firstSolveProblemElement = profileService.findFirstSolveElement(driver, waitDriver);
+            String problemNumber = problemExtractor.extractProblemNumber(firstSolveProblemElement);
+            String extension = problemExtractor.extractProblemExtension(firstSolveProblemElement);
+            String link = problemExtractor.extractProblemSourceCode(firstSolveProblemElement);
+            parse.connectAndInitCookies(link, cookies);
+            String sourceCode = problemExtractor.extractorSourceCode(parse);
+
+            Problem problem = new Problem.Builder()
+                    .problemNumber(problemNumber)
+                    .algorithmTag(algorithmTag)
+                    .extension(extension)
+                    .sourceCode(sourceCode)
+                    .build();
+            ret.add(problem);
+        }
+        return new ProblemInfoDto(ret);
     }
 }
--- src/main/java/application/service/TaskManger.java
+++ src/main/java/application/service/TaskManger.java
@@ -1,9 +1,19 @@
 package application.service;
 
+import application.dto.PageElements;
+import application.dto.ProblemInfoDto;
+import domain.algorithm.problem.Problem;
+import domain.cookie.SeleniumCookie;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface TaskManger {
     void login();
     void start();
     void quit();
     void enterMyPage();
     void enterMysSolvedPage();
+    List<String> extractProblemLink(SeleniumCookie cookie);
+    ProblemInfoDto extractProblemInfoDto(List<String> problemInfo, SeleniumCookie cookies) throws IOException;
 }

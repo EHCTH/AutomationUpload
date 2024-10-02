2d1ae7d105bdd5e5ee437cf53c00bf5dae155d33
EHCTH <lees90896@gmail.com>
2024-10-02 12:03:15 +0900

    feat: ProblemExtractor add * feat: ProblemExtractor add

--- src/main/java/application/service/ServiceTaskFactory.java
+++ src/main/java/application/service/ServiceTaskFactory.java
@@ -4,6 +4,7 @@ import application.service.login.LoginService;
 import application.service.login.LoginServiceManage;
 import application.service.profile.ProfileManage;
 import application.service.profile.ProfileService;
+import infrastructure.parse.ProblemExtractor;
 import infrastructure.selenium.Driver.DriverController;
 import infrastructure.selenium.Driver.DriverSetting;
 import infrastructure.selenium.Driver.WaitDriver;
@@ -23,11 +24,13 @@ public class ServiceTaskFactory {
         DriverController driver = driverSetting.getDriver();
         WaitDriverController waitDriver = new WaitDriver(driver.getWebDriver(), Duration.ofSeconds(10));
         ProfileService profileService = new ProfileService();
+        ProblemExtractor problemExtractor = new ProblemExtractor();
         return new ServiceTask(
                 loginService,
                 driver,
                 waitDriver,
-                profileService
+                profileService,
+                problemExtractor
         );
     }
 }

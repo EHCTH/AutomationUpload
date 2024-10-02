7f43c7e365253aa9d09ea5b033db215ee457724e
EHCTH <lees90896@gmail.com>
2024-10-02 16:33:53 +0900

    refactor: class 의존성 추가 * refactor: solid 준수 의존성 추가

--- src/main/java/application/service/ServiceTaskFactory.java
+++ src/main/java/application/service/ServiceTaskFactory.java
@@ -2,8 +2,10 @@ package application.service;
 
 import application.service.login.LoginService;
 import application.service.login.LoginServiceManage;
-import application.service.profile.ProfileManage;
 import application.service.profile.ProfileService;
+import infrastructure.parse.CookieManager;
+import infrastructure.parse.LinkExtractor;
+import infrastructure.parse.Parse;
 import infrastructure.parse.ProblemExtractor;
 import infrastructure.selenium.Driver.DriverController;
 import infrastructure.selenium.Driver.DriverSetting;
@@ -15,22 +17,24 @@ import java.time.Duration;
 
 public class ServiceTaskFactory {
     public static TaskManger createServiceTask(WebDriver webDriver) {
-        LoginServiceManage loginService = new LoginService(
-                new UserService()
-        );
-        DriverSetting driverSetting = new DriverSetting(
-                webDriver
-        );
+        DriverSetting driverSetting = new DriverSetting(webDriver);
         DriverController driver = driverSetting.getDriver();
         WaitDriverController waitDriver = new WaitDriver(driver.getWebDriver(), Duration.ofSeconds(10));
-        ProfileService profileService = new ProfileService();
-        ProblemExtractor problemExtractor = new ProblemExtractor();
+        LoginServiceManage loginService = new LoginService(
+                new UserService(),
+                driver,
+                waitDriver
+
+        );
+        Parse parse = new Parse(new CookieManager());
+        ProfileService profileService = new ProfileService(driver, waitDriver);
+        LinkExtractor linkExtractor = new LinkExtractor(driver, waitDriver, parse);
+        ProblemExtractor problemExtractor = new ProblemExtractor(parse);
         return new ServiceTask(
                 loginService,
-                driver,
-                waitDriver,
                 profileService,
-                problemExtractor
+                problemExtractor,
+                linkExtractor
         );
     }
 }

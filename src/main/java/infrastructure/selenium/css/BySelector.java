0e765d4712a5ae65a07bac5aa6582d99371b65b5
EHCTH <lees90896@gmail.com>
2024-10-02 11:45:28 +0900

    feat: css 추가 * extractor css add

--- src/main/java/infrastructure/selenium/css/BySelector.java
+++ src/main/java/infrastructure/selenium/css/BySelector.java
@@ -7,7 +7,16 @@ public enum BySelector {
     LOGIN_SUBMIT_BUTTON("#submit_button"),
     USER_PAGE("a.username"),
     USER_SOLVED_PAGE("#statics > tbody > tr:nth-child(2) > td > a"),
-    USER_SOLVED_PROBLEM_PAGE("ul.pagination a");
+    USER_SOLVED_PROBLEM_PAGE("ul.pagination a"),
+
+    PROBLEM_SET("#problemset > tbody > tr > td:nth-child(2) > a"),
+    MY_SUBMIT("ul.problem-menu > li:nth-child(7) > a"),
+    ALGORITHM_TAG("ul.spoiler-list a.spoiler-link"),
+    STATUS_TABLE("#status-table > tbody > tr"),
+    RESULT_TABLE("td:nth-child(4) > span.result-text"),
+    PROBLEM_NUMBER("td:nth-child(3) > a"),
+    PROBLEM_SOLVE_LANGUAGE("td:nth-child(7) > a"),
+    SOURCE_CODE("textarea.no-mathjax.codemirror-textarea");
 
     private final String selector;
     BySelector(String selector) {
@@ -33,5 +42,29 @@ public enum BySelector {
     }
     public static String getSolvedProblemPage() {
         return USER_SOLVED_PROBLEM_PAGE.selector;
+    }
+    public static String getProblemSet() {
+        return PROBLEM_SET.selector;
+    }
+    public static String getMySubmit() {
+        return MY_SUBMIT.selector;
+    }
+    public static String getAlgorithmTag() {
+        return ALGORITHM_TAG.selector;
+    }
+    public static String getStatusTable() {
+        return STATUS_TABLE.selector;
+    }
+    public static String getResultTable() {
+        return RESULT_TABLE.selector;
+    }
+    public static String getProblemNumber() {
+        return PROBLEM_NUMBER.selector;
+    }
+    public static String getSolveLanguage() {
+        return PROBLEM_SOLVE_LANGUAGE.selector;
+    }
+    public static String getSourceCode() {
+        return SOURCE_CODE.selector;
     }
 }
